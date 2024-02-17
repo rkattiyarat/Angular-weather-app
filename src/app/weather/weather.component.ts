63811de5b2ada7096cdc4a458c6d1314938aa013
rose <aom_bnn@hotmail.com>
2024-02-17 16:23:24 -0500

    color invalid input

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -9,14 +9,22 @@ import { WeatherService } from '../weather.service';
 export class WeatherComponent {
   city: string ='';
   weatherData: any;
+  isEmty: boolean = false;
   constructor(private weatherService:WeatherService) { }
 
   ngOnInit() {}
 
   getWeather() {
+  if(this.city === '') {
+    this.isEmty = true;
+      // color the input field red
+  } else {
+    this.isEmty = false;
+  }
+  if(!this.isEmty) {
     this.weatherService.getWeather(this.city).subscribe(data => {this.weatherData = data; console.log(data);});
     // clear the input field
     this.city = '';
   }
-
+}
 }
