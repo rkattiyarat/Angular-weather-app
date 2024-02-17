d7dfeb17ebe1d350e4ffa56910c7f087b8475a70
rose <aom_bnn@hotmail.com>
2024-02-16 19:13:29 -0500

    first commit

---
+++ src/app/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  apiKey = 'd1381a691fd9d803e38585fb280157d5';
+
+
+  constructor(private http:HttpClient) {
+   
+   }
+   getWeather(city: string) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
+    return this.http.get(url);
+  }
+}
