09b0fa70960fd36e701ccf9695659756d0f402d2
rose <aom_bnn@hotmail.com>
2024-02-18 19:49:32 -0500

    dropdown menu

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -13,9 +13,20 @@ export class WeatherComponent {
   isEmty: boolean = false;
   roundTemp: number = 0;
   iconUrl: string = 'http://openweathermap.org/img/wn/';
+  temperatureUnit: string = 'Celsius';
   constructor(private weatherService:WeatherService) { }
 
   ngOnInit() {}
+
+  toggleTemperatureUnit() {
+    if (this.temperatureUnit === 'Celsius') {
+      this.getWeatherInFahrenheit();
+      this.temperatureUnit = 'Fahrenheit';
+    } else {
+      this.getWeather();
+      this.temperatureUnit = 'Celsius'; 
+    }
+  }
 
   getWeather() {
   if(this.city === '') {
@@ -36,10 +47,32 @@ export class WeatherComponent {
   }
 }
 
+getWeatherInFahrenheit() {
+  if(this.city === '') {
+    this.isEmty = true;
+      // color the input field red
+  } else {
+    this.isEmty = false;
+  }
+  if(!this.isEmty) {
+    this.weatherService.getWeather(this.city).subscribe(data => {
+      this.weatherData = data;
+      this.roundTemp = Math.round((this.weatherData.main.temp * 9/5) + 32);
+      this.iconUrl = this.iconUrl + this.weatherData.weather[0].icon + '.png';
+      console.log(data);
+    });
+    // clear the input field
+    this.city = '';
+  }
+
+}
+
 onReset() {
   this.city = '';
   this.weatherData = null;
   this.roundTemp = 0;
   this.iconUrl = 'http://openweathermap.org/img/wn/';
   }
+
+
 }
